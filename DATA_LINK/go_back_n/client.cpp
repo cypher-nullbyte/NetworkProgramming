a6c4dbc7aa929b78a24dc3bdb75cd7604420295c
cYpHeR <github.com/cypher-nullbyte>
2021-03-22 23:25:44 +0530

    Data Link ARQ Added

---
+++ DATA_LINK/go_back_n/client.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+#include<stdio.h>
+#include<strings.h>
+#include<string.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<unistd.h>
+ 
+#define cls() printf("33[H33[J (cleaned buffer)")
+                        //structure definition for accepting the packets.
+
+struct frame
+
+{
+ int packet[40];
+};
+            //structure definition for constructing the acknowledgement frame
+
+struct ack
+
+{
+ int acknowledge[40];
+};
+ 
+
+int main()
+{
+ int clientsocket;
+ sockaddr_in serveraddr;
+ socklen_t len;
+ hostent * server;
+ frame f1;
+ int windowsize,totalpackets,totalframes,i=0,j=0,framesreceived=0,k,buffer,l;
+ ack acknowledgement;
+ char req[50];
+ 
+ clientsocket=socket(AF_INET,SOCK_DGRAM,0);
+ 
+ bzero((char*)&serveraddr,sizeof(serveraddr));
+ serveraddr.sin_family=AF_INET;
+ serveraddr.sin_port=htons(5018);
+ server=gethostbyname("127.0.0.1");
+ bcopy((char*)server->h_addr,(char*)&serveraddr.sin_addr.s_addr,sizeof(server->h_addr));
+ 
+                                                //establishing the connection.
+ printf("\nSending request to the Server.\n");
+ sendto(clientsocket,"HI I AM CLIENT.",sizeof("HI I AM CLIENT."),0,(sockaddr*)&serveraddr,sizeof(serveraddr));
+ 
+ printf("\nWaiting for reply.\n");
+ recvfrom(clientsocket,req,sizeof(req),0,(sockaddr*)&serveraddr,&len);
+ printf("\nThe server has send:\t%s\n",req);
+ 
+                                                //accepting window size from the user.
+ printf("\nEnter the window size:\t");
+ scanf("%d",&windowsize);
+                                                //sending the window size.
+ printf("\n\nSending the window size.\n");
+ sendto(clientsocket,(char*)&windowsize,sizeof(windowsize),0,(sockaddr*)&serveraddr,sizeof(serveraddr));
+ cls();
+ 
+                                                //collecting details from server.
+ printf("\nWaiting for the server response.\n");
+ recvfrom(clientsocket,(char*)&totalpackets,sizeof(totalpackets),0,(sockaddr*)&serveraddr,&len);
+ printf("\nThe total packets are:\t%d\n",totalpackets);
+ sendto(clientsocket,"RECEIVED.",sizeof("RECEIVED."),0,(sockaddr*)&serveraddr,sizeof(serveraddr));
+ 
+ recvfrom(clientsocket,(char*)&totalframes,sizeof(totalframes),0,(sockaddr*)&serveraddr,&len);
+ printf("\nThe total frames/windows are:\t%d\n",totalframes);
+ sendto(clientsocket,"RECEIVED.",sizeof("RECEIVED."),0,(sockaddr*)&serveraddr,sizeof(serveraddr));
+ 
+                                                //starting the process.
+ printf("\nStarting the process of receiving.\n");
+ while(i<totalpackets)
+ {
+                                                //initialising the receive buffer.
+   printf("\nInitialising the receive buffer.\n");
+   printf("\nThe expected frame is %d with packets:  ",framesreceived);
+   j=0;
+   buffer=i;
+   while(j<windowsize && i<totalpackets)
+   {
+    printf("%d  ",i%(windowsize+1)); 
+    i++;
+    j++; 
+   }
+ 
+   printf("\n\nWaiting for the frame.\n");
+                                                //accepting the frame.  
+   recvfrom(clientsocket,(char*)&f1,sizeof(f1),0,(sockaddr*)&serveraddr,&len);
+   printf("\nReceived frame %d\n\nEnter -1 to send negative acknowledgement for the following packets.\n",framesreceived);
+ 
+                                                //constructing the acknowledgement frame.
+   j=0;
+   l=buffer;
+   k=0;
+   while(j<windowsize && l<totalpackets)
+   {
+    printf("\nPacket: %d\n",f1.packet[j]%(windowsize+1)); 
+                                                //accepting acknowledgement from the user.
+    scanf("%d",&acknowledgement.acknowledge[j]);
+ 
+    if(acknowledgement.acknowledge[j]==-1)
+    {
+     if(k==0)
+     {
+      i=f1.packet[j];
+      k=1; 
+     } 
+    }
+    j++;
+    l++;
+   }
+   framesreceived++;
+                                               
+                                                //sending acknowledgement to the server.
+ sendto(clientsocket,(char*)&acknowledgement,sizeof(acknowledgement),0,(sockaddr*)&serveraddr,sizeof(serveraddr));
+  cls();
+ }
+ 
+ printf("\nAll frames received successfully.\n\nClosing connection with the server.\n");
+ close(clientsocket);
+}
--- TCP/TCP_server.c
+++ TCP/TCP_server.c
@@ -4,30 +4,6 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 
-<<<<<<< HEAD
-#include<netinet/in.h>
-#include<unistd.h>
-// #include<fcntl.h>
-
-
-int main()
-{
-    char server_message[256]="You have reached the server!";
-    //create the server socket;
-    int server_socket;
-    server_socket=socket(AF_INET,SOCK_STREAM,0);
-
-    //define the server address;
-    struct sockaddr_in server_address;
-    server_address.sin_family=AF_INET;
-    server_address.sin_port=htons(9002);
-    server_address.sin_addr.s_addr=INADDR_ANY;
-
-    //bind the socket to our speified IP and port
-    bind(server_socket,(struct sockaddr*) &server_address,sizeof(server_address));
-    listen(server_socket,5);
-    
-=======
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<unistd.h>
@@ -48,6 +24,7 @@ int main(int argc, char* argv[])
     //CREATE THE SERVER SOCKET
     int server_socket;
     server_socket=socket(AF_INET,SOCK_STREAM,0);
+    printf("%d\n",server_socket);
     
     //define the server address
     struct sockaddr_in server_address;
@@ -59,21 +36,18 @@ int main(int argc, char* argv[])
     //bind the socket to our specified IP and port
     bind(server_socket,(struct sockaddr*) &server_address,sizeof(server_address));
     listen(server_socket,5); 
->>>>>>> c370573... Till checksum
 
     int client_socket;
-    client_socket=accept(server_socket,NULL,NULL);
-
+    // client_socket=accept(server_socket,NULL,NULL);
+    struct sockaddr_in client_address;
+    socklen_t cliLen=sizeof(client_address);
+    client_socket=accept(server_socket,(struct sockaddr*)&client_address,&cliLen);
+    printf("%d\n",client_socket);
     //send the message
     send(client_socket,server_message,sizeof(server_message),0);
 
     //close the connection
-<<<<<<< HEAD
-
-    close(server_socket);
-=======
     close(server_socket);
     
->>>>>>> c370573... Till checksum
     return 0;
 }
